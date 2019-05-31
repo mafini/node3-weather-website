a13221771b3e25b9d849a63edff784d8a05e7daa
Mohammedi Afini <mohd.afini@gmail.com>
2019-05-31 10:49:15 -0400

    Removed unwanted console.log statements

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,8 +4,7 @@ const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoibW9kaXN1YnMiLCJhIjoiY2p2c2NpdTk4MGc2ZTN5bWZ3eWE3aTRybSJ9._pHYmTWhLGTZkI6b-1sGPQ&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
-        console.log(error);
-        console.log(body);
+
         if (error) {
             callback('Unable to connect to location services!', undefined)
         } else if (body.features.length === 0) {
